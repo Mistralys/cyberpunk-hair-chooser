5280681c4c354aa838bcdea6c2c5cea2721e5280
Mistralys <s.mordziol@gmail.com>
2024-03-07 20:42:20 +0100

    Added config file.

---
+++ config/paths.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\CPHairChooser;
+
+const MODS_DOWNLOAD_FOLDER = 'C:\Vortex Downloads';
+const MODS_EXTRACT_FOLDER = 'C:\Cyberpunk Hairs\Extract';
+const BUILT_MODS_FOLDER = 'C:\Cyberpunk Hairs\Build';
